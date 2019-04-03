cebc09dc50388028c64f24c9b640a3c66dcc6014
Benjamin Schneider <mail@benjaminschneider.ch>
2019-04-03 12:46:46 +0200

    Remove unused example code

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-var host = "http://tpb.pirateparty.org.uk";
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
          return {redirectUrl: "https://sci-hub.tw/" + details.url};
